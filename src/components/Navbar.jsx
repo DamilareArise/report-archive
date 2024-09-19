d57d228624661192970de4dabe1ba0a60e38ad3a
Onifade Joshua <onifadjosh@gmail.com>
2024-09-19 08:12:21 +0100

    Updated Login.jsx, Navbar.jsx, AdminDisplayThesis.jsx, and UserDisplayThesis.jsx with minor changes to code formatting, syntax, and added/removed assets.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,50 @@
 // import React from 'react'
 import { useNavigate } from "react-router-dom";
-import logout from "./../assets/logout.png"
+import logout from "./../assets/logout.png";
 import { getAuth, signOut } from "firebase/auth";
 
 const Navbar = ({ user }) => {
-  const navigate = useNavigate()
-  const auth = getAuth()  
+  const navigate = useNavigate();
+  const auth = getAuth();
   const logoutUser = () => {
-    signOut(auth).then(() => {
-      console.log("Sign-out successful.")
-      navigate('/')
-    }).catch((error) => {
-      // An error happened.
-    });
-  }
-  
+    signOut(auth)
+      .then(() => {
+        console.log("Sign-out successful.");
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
   return (
     <div className="bg-[#020252] py-[21px] flex justify-end items-center px-[24px] md:px-[50px]">
       {/* Display default or user's photo */}
       <div className="w-[60px] h-[60px] bg-[#A09E9E] rounded-[400px] mr-[10px] lg:mr-[10px]">
         {user && user.photoURL ? (
-          <img src={user.photoURL} alt="User avatar" className="rounded-[400px]" />
+          <img
+            src={user.photoURL}
+            alt="User avatar"
+            className="rounded-[400px]"
+          />
         ) : (
           <div className="w-full h-full bg-gray-300 rounded-full" />
         )}
       </div>
-       {/* Display user email */}
-       <p className="mr-[20px] lg:mr-[40px] text-white">
+      {/* Display user email */}
+      <p className="mr-[20px] lg:mr-[40px] text-white">
         {user ? user.email : "Guest"}
       </p>
 
-      <div className="flex items-center gap-[5px] cursor-pointer" onClick={logoutUser}>
-          <p className="text-white">Logout</p>
-          <img src={logout} alt="" />
+      <div
+        className="flex items-center gap-[5px] cursor-pointer"
+        onClick={logoutUser}
+      >
+        <p className="text-white">Logout</p>
+        <img src={logout} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/AdminDisplayThesis.jsx
+++ src/pages/AdminDisplayThesis.jsx
@@ -2,20 +2,19 @@
 import del from "./../assets/delete.png";
 import edit from "./../assets/edit.png";
 import upload from "./../assets/upload.png";
+import arrowBack from "./../assets/backw.svg";
 import { getDatabase, ref, onValue } from "firebase/database";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const AdminDisplayThesis = ({ app }) => {
-
   const [thesisList, setThesisList] = useState([]);
   const [user, setUser] = useState(null);
 
   const auth = getAuth();
   const database = getDatabase(app);
   const navigate = useNavigate();
-
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -45,12 +44,32 @@ const AdminDisplayThesis = ({ app }) => {
     });
   }, [auth, navigate]);
 
-
-
   return (
     <div>
       <div className="flex">
-        <div className="hidden lg:block w-[20%] h-screen bg-[#020252] fixed"></div>
+        <div className="hidden lg:block w-[20%] h-screen bg-[#020252] fixed pl-[26px]">
+        <Link to={"/admindashboard"} className="self-start md:self-center ">
+            <img src={arrowBack} alt="Go Back" className="pt-[50px] pb-[37px]"/>
+          </Link>
+          <div className="w-[60px] h-[60px] bg-[#A09E9E] rounded-[400px] mr-[10px] lg:mr-[10px] mb-[18px]">
+            {user && user.photoURL ? (
+              <img
+                src={user.photoURL}
+                alt="User avatar"
+                className="rounded-[400px]"
+              />
+            ) : (
+              <div className="w-full h-full bg-gray-300 rounded-full" />
+            )}
+          </div>
+          {/* Display user email */}
+          <p className="font-[600] text-[20px] leading-[24.2px] text-white pb-2">
+              Name: <p className="py-2 font-[400] uppercase text-white">{user ? user.displayName : ''}</p>
+            </p>
+          <p className="mr-[20px] lg:mr-[40px] font-[600] text-[20px] text-white">
+            Email: {user ? user.email : "Guest"}
+          </p>
+        </div>
 
         <div className="flex w-[100%]">
           <div className="hidden lg:block w-[20%]"></div>
@@ -64,7 +83,9 @@ const AdminDisplayThesis = ({ app }) => {
                     <th className="pt-[45px] pb-[20px] w-[30%]">Title</th>
                     <th className="pt-[45px] pb-[20px] w-[20%]">Author</th>
                     {/* <th className="pt-[45px] pb-[20px] w-[10%] xl:w-[20%]">Year</th> */}
-                    <th className="pt-[45px] pb-[20px] w-[15%]">Last modi...</th>
+                    <th className="pt-[45px] pb-[20px] w-[15%]">
+                      Last modi...
+                    </th>
                     <th className="pt-[45px] pb-[20px] w-[20%] xl:w-[15%]"></th>
                   </tr>
                 </thead>
@@ -80,7 +101,9 @@ const AdminDisplayThesis = ({ app }) => {
                         Esther Oluwaferanmi
                       </td>
                       <td className="text-center pt-[26px] pb-[18px]">2023</td>
-                      <td className="text-center pt-[26px] pb-[18px]">July, 24</td>
+                      <td className="text-center pt-[26px] pb-[18px]">
+                        July, 24
+                      </td>
                       <td className="pt-[35px] pb-[18px] flex flex-col md:flex-row gap-[10px] justify-center items-center h-full">
                         <img
                           src={upload}
@@ -106,34 +129,42 @@ const AdminDisplayThesis = ({ app }) => {
                       );
                       const documentData = thesis[documentKey];
                       return (
-                      <tr
-                        key={index}
-                        className="bg-[#F4F4F4] mb-[15px] shadow-md shadow-[#00000040]"
-                      >
-                        <td className="pt-[26px] pb-[18px] text-center">{index + 1}</td>
-                        <td className="pt-[26px] pb-[18px] text-center">{documentData.fileName}</td>
-                        <td className="pt-[26px] pb-[18px] text-center">{documentData.author}</td>
-                        {/* <td className="pt-[26px] pb-[18px]">{thesis.year}</td> */}
-                        <td className="pt-[26px] pb-[18px] text-center">{documentData.date_created}</td>
-                        <td className="pt-[35px] pb-[18px] flex gap-[10px] justify-center items-center h-full">
-                          <img
-                            src={upload}
-                            alt=""
-                            className="p-[5px] border-[1px] border-[#000000] rounded-[8px]"
-                          />
-                          <img
-                            src={edit}
-                            alt=""
-                            className="/p-[5px] border-[1px] border-[#000000] rounded-[8px]"
-                          />
-                          <img
-                            src={del}
-                            alt=""
-                            className="/p-[5px] border-[1px] border-[#000000] rounded-[8px]"
-                          />
-                        </td>
-                      </tr>
-                      )
+                        <tr
+                          key={index}
+                          className="bg-[#F4F4F4] mb-[15px] shadow-md shadow-[#00000040]"
+                        >
+                          <td className="pt-[26px] pb-[18px] text-center">
+                            {index + 1}
+                          </td>
+                          <td className="pt-[26px] pb-[18px] text-center">
+                            {documentData.fileName}
+                          </td>
+                          <td className="pt-[26px] pb-[18px] text-center">
+                            {documentData.author}
+                          </td>
+                          {/* <td className="pt-[26px] pb-[18px]">{thesis.year}</td> */}
+                          <td className="pt-[26px] pb-[18px] text-center">
+                            {documentData.date_created}
+                          </td>
+                          <td className="pt-[35px] pb-[18px] flex gap-[10px] justify-center items-center h-full">
+                            <img
+                              src={upload}
+                              alt=""
+                              className="p-[5px] border-[1px] border-[#000000] rounded-[8px]"
+                            />
+                            <img
+                              src={edit}
+                              alt=""
+                              className="/p-[5px] border-[1px] border-[#000000] rounded-[8px]"
+                            />
+                            <img
+                              src={del}
+                              alt=""
+                              className="/p-[5px] border-[1px] border-[#000000] rounded-[8px]"
+                            />
+                          </td>
+                        </tr>
+                      );
                     })
                   )}
                 </tbody>
